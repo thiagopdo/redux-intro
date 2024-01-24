3ae33380dc14b53d5c8752ae236e9ec0dc087777
thiagopdo <thiago.falconer@gmail.com>
2024-01-24 16:33:26 -0300

    FIrst commit to use REDUX and usereducer

---
+++ src/store.js
@@ -0,0 +1,33 @@
+import { createStore } from "redux";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    // Adicione casos de ação conforme necessário
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.load > 0) return state;
+      return { ...state, load: action.payload };
+    case "account/payLoan":
+      return {
+        ...state,
+        load: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 500 });
